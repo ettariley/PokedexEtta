ba3665542df56803e881944080b2c2d6631789c8
Etta Baker <etta.r.baker@gmail.com>
2022-02-12 18:33:56 -0500

    Created pokemonList array and added in 3 pokemon.

---
+++ js/script.js
@@ -0,0 +1,6 @@
+// Add three pokemon and relevant information to an array.
+let pokemonList = [
+    { name: 'Butterfree', height: 1.1, type: ['bug', 'flying'] },
+    { name: 'Oddish', height: 0.5, type: ['grass', 'poison'] },
+    { name: 'Haunter', height: 1.6, type: ['ghost', 'poison'] }
+];
